fb205a0c9f00b0cb069ed6984679dbea83f17ec8
MalasaniPoojitha <139150384+MalasaniPoojitha@users.noreply.github.com>
2023-07-12 21:53:54 +0530

    Add files via upload

---
+++ src/food/Restaurant.java
@@ -0,0 +1,12 @@
+package food;
+
+public class Restaurant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Itemprices i=new Itemprices();
+		i.setprices();
+
+	}
+
+}
